0bb523ff5ff92c18c89cb5e9091050832f14b377
Jason Huang <jason.huang@technexion.com>
2025-08-13 09:40:57 +0800

    Release vizionviewer v25.08.1, vizionsdk v25.08.1, vizion-ctl v25.08.1
    VizionViewer:
    - New UI design
    - Adjustable theme: Blue, Purple, Green
    - Customizable logo
    - Customizable control panel layout
    - Start / pause streaming
    
    VizionSDK:
    - Added eHDR-related properties for AR0821 and AR0822 sensor
    - Fixed GetCurrentGain function implementation

--- vizionsdk/usr/lib/cmake/vizionsdk/vizionsdkTargets-release.cmake
+++ vizionsdk/usr/lib/cmake/vizionsdk/vizionsdkTargets-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "vizionsdk::VizionSDK" for configuration "Release"
 set_property(TARGET vizionsdk::VizionSDK APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(vizionsdk::VizionSDK PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libVizionSDK.so.25.6.1"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libVizionSDK.so.25.8.1"
   IMPORTED_SONAME_RELEASE "libVizionSDK.so.25"
   )
 
 list(APPEND _cmake_import_check_targets vizionsdk::VizionSDK )
-list(APPEND _cmake_import_check_files_for_vizionsdk::VizionSDK "${_IMPORT_PREFIX}/lib/libVizionSDK.so.25.6.1" )
+list(APPEND _cmake_import_check_files_for_vizionsdk::VizionSDK "${_IMPORT_PREFIX}/lib/libVizionSDK.so.25.8.1" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
